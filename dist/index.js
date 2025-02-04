da8275006b0b8e8b5c19460d89b6c2c9f253b016
Harish <hshejokar@gmail.com>
2025-02-04 14:54:30 +0530

    ts build check

--- dist/index.js
+++ dist/index.js
@@ -5,9 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
-app.get('/', (req, res) => {
-    // Send a response to the client
-    res.send('Hello, TypeScript + Node.js + Express!');
+app.use(`/test`, (req, res) => {
+    res.send('<h1>test!</h1>');
+});
+app.use('/', (req, res) => {
+    res.send('<h1>Hello, TypeScript + Node.js + Express!</h1>');
 });
 const port = "6001";
 app.listen(port, () => {
