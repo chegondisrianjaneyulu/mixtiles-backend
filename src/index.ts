72a6c83565d0616be3f539e21cba3437c3cccd66
Harish <hshejokar@gmail.com>
2025-02-04 14:57:09 +0530

    update-1

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,15 @@ import express,{Request, Response} from "express";
 
 const app = express();
 
-app.get('/', (req:Request, res:Response) => {
+app.use(`/test`, (req:Request, res:Response)=> {
+  res.send('<h1>test!</h1>');
+})
+
+app.use('/', (req:Request, res:Response) => {
     res.send('<h1>Hello, TypeScript + Node.js + Express!</h1>');
   });
+
+
   
   const port = "6001"
 
